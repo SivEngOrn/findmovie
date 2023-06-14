da29be4e14eb6528aa13649f9fa46dc6919e8a6e
sivengo <sivengorn@gmail.com>
2023-06-14 22:33:59 +0700

    update url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 // 301670b1
 
-const API_URL = "http://www.omdbapi.com/?apikey=301670b1";
+const API_URL = "https://www.omdbapi.com/?apikey=301670b1";
 
 
 
